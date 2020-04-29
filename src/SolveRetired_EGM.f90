b927db885d18eea0058296dd47b32d5cbf5e0faf
SerhiyStp <sstepanchuk@gmail.com>
2020-04-29 14:36:47 +0100

    Creates dev type

--- src/SolveRetired_EGM.f90
+++ src/SolveRetired_EGM.f90
@@ -1,17 +1,26 @@
 module SolveRetired_EGM
 use Model_Parameters
     implicit none
-    real(8), dimension(:,:,:,:,:,:,:,:,:,:,:), allocatable, target :: dev_ret
-    real(8), dimension (:,:,:,:,:,:,:), allocatable, target :: devs_ret
+
+    !real(8), dimension(:,:,:,:,:,:,:,:,:,:,:), allocatable :: dev_ret
+    !real(8), dimension (:,:,:,:,:,:,:), allocatable :: devs_ret
     real(8), dimension(:,:,:,:,:,:,:,:,:,:,:), allocatable, target :: evv_ret
     real(8), dimension (:,:,:,:,:,:,:), allocatable, target :: evvs_ret
+
+    type dev
+        real(8) :: f(nk)
+        integer :: ilo, ihi
+    end type dev
+
+    type(dev) :: dev_ret(2,2,nexp,nexp,na,nu,na,nu,nfc,nfc)
+    type(dev) :: devs_ret(2,2,nexp,na,nu,nfc)
 
 contains
 
     subroutine Initialize_Retired()
 
-        allocate(dev_ret(2,2,nk,nexp,nexp,na,nu,na,nu,nfc,nfc))
-        allocate(devs_ret(2,2,nk,nexp,na,nu,nfc))
+        !allocate(dev_ret(2,2,nk,nexp,nexp,na,nu,na,nu,nfc,nfc))
+        !allocate(devs_ret(2,2,nk,nexp,na,nu,nfc))
         allocate(evv_ret(2,2,nk,nexp,nexp,na,nu,na,nu,nfc,nfc))
         allocate(evvs_ret(2,2,nk,nexp,na,nu,nfc))
 
@@ -35,6 +44,7 @@ contains
         integer :: ixm_plt, iam_plt, ium_plt, ixf_plt, iaf_plt, iuf_plt
         integer :: ifcm_plt, ifcf_plt
         real(8) :: evvs
+        real(8) :: df
         integer :: iu2
 
         do j = 1, 2
@@ -47,24 +57,6 @@ contains
             end do
         end do
 
-        !do ix = 1, nexp
-        !do ia = 1, na
-        !do iu = 1, nu
-        !do ifc = 1, nfc
-        !do j = 1, 2
-        !do ik = 1, nk
-            !evvs = 0.0d0
-            !do iu2 = 1, nu
-                !evvs = evvs &
-                    !+ trans_u(j,iu,iu2)*vs_ret(2,j,ik,ix,ia,iu2,i_t,ifc)
-            !end do
-            !evvs_ret(2,j,ik,ix,ia,iu,ifc) = evvs
-        !end do
-        !end do
-        !end do
-        !end do
-        !end do
-        !end do
         evvs_ret(1,:,:,:,:,:,:) = vs_ret(1,:,:,:,:,:,i_t,:)
 
         eps = 1.0d-5
@@ -82,8 +74,8 @@ contains
                 k1 = k_grid(ik) + eps
                 v0 = LinInterp(k0,k_grid,ev_k_ptr,nk)
                 v1 = LinInterp(k1,k_grid,ev_k_ptr,nk)
-                devs_ret(i1,i2,ik,ix,ia,iu,ifc) = &
-                    (v1 - v0)/(2d0*eps)
+                df = (v1 - v0)/(2d0*eps)
+                devs_ret(i1,i2,ix,ia,iu,ifc)%f(ik) = df
             end do
         end do
         end do
@@ -109,8 +101,8 @@ contains
                 k1 = k_grid(ik) + eps
                 v0 = LinInterp(k0,k_grid,ev_k_ptr,nk)
                 v1 = LinInterp(k1,k_grid,ev_k_ptr,nk)
-                dev_ret(i1,i2,ik,ixm,ixf,iam,ium,iaf,iuf,ifcm,ifcf) = &
-                    (v1 - v0)/(2d0*eps)
+                df = (v1 - v0)/(2d0*eps)
+                dev_ret(i1,i2,ixm,ixf,iam,ium,iaf,iuf,ifcm,ifcf)%f(ik) = df
             end do
         end do
         end do
@@ -127,11 +119,12 @@ contains
         ia_plt = 1
         iu_plt = 1
         ifc_plt = 1
-        dev_plot = devs_ret(1,1,:,ix_plt,ia_plt,iu_plt,ifc_plt)
+        dev_plot = devs_ret(1,1,ix_plt,ia_plt,iu_plt,ifc_plt)%f
 
         call plt%initialize(grid=.true.,xlabel='k',&
             title='MU',legend=.true.)
-        call plt%add_plot(k_grid,dev_plot,label='MU (single)',linestyle='b-o',markersize=5,linewidth=2)
+        call plt%add_plot(k_grid,dev_plot,label='MU (single)',&
+            linestyle='b-o',markersize=5,linewidth=2)
 
         ixm_plt = 1
         ixf_plt = 1
@@ -141,9 +134,12 @@ contains
         iuf_plt = 1
         ifcm_plt = 1
         ifcf_plt = 1
-        dev_plot = dev_ret(1,1,:,ixm_plt,ixf_plt,iam_plt,ium_plt,iaf_plt,iuf_plt,ifcm_plt,ifcf_plt)
+        dev_plot = &
+            dev_ret(1,1,ixm_plt,ixf_plt,iam_plt,ium_plt,iaf_plt,iuf_plt,&
+                ifcm_plt,ifcf_plt)%f
 
-        call plt%add_plot(k_grid,dev_plot,label='MU (married)',linestyle='r--',markersize=5,linewidth=2)
+        call plt%add_plot(k_grid,dev_plot,label='MU (married)',&
+            linestyle='r--',markersize=5,linewidth=2)
         call plt%savefig('dev.png', pyfile='dev.py')
 
     end subroutine Compute_EV_DEV
